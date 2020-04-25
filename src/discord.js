a195ac16ac7988c859c17d10221d9cacd8ca9f74
antoine <21809219@etu.unicaen.fr>
2020-04-25 04:11:22 +0200

    fin du dev

--- src/discord.js
+++ src/discord.js
@@ -1,37 +1,197 @@
 const steamApi = require('./steam.js')
-const Discord = require('discord.js');
+const discord = require('discord.js')
 const apiKey = require('../apiKey.json')
+const fs = require('fs')
+const dataSql = fs.readFileSync("./sql.sql").toString()
+const Database = require('better-sqlite3');
 
-const client = new Discord.Client();
-
-client.login(apiKey.discord);
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+const db = new Database('SteamBanMonitor.db', {
+    verbose: console.log
 });
 
-client.on('message', msg => {
-    if (!msg.content.startsWith('!info'))
-        return
+db.exec(dataSql)
 
+const discordClient = new discord.Client()
+discordClient.login(apiKey.discord);
+
+
+
+discordClient.on('ready', () => {
+    console.log(`Logged in as ${discordClient.user.tag}!`)
+    banCheck()
+    setInterval(banCheck, 60000)
+});
+
+discordClient.on('message', msg => {
+    switch (msg.content.split(" ")[0]) {
+        case '!infos':
+            infos(msg);
+            break;
+        case '!watchadd':
+            watchadd(msg);
+            break;
+        case '!watchrm':
+            watchrm(msg);
+            break;
+        case '!watchls':
+            watchls(msg);
+            break;
+        case '!banls':
+            banls(msg);
+            break;
+        case '!help':
+            help(msg)
+            break;
+    }
+
+});
+
+
+function infos(msg) {
     let userLink = msg.content.split(" ")[1]
+    sendInfoLink(msg, userLink)
+}
 
-
-    steamApi.getInfos(userLink).then(summary => {
-            console.log(summary)
-            const embed = new Discord.MessageEmbed()
-                .setColor('#ff0000')
-                .setTitle(summary.nickname)
-                .setURL(summary.url)
-                .addFields({
-                    name: 'id64',
-                    value: summary.steamID,
-                    inline: true
-                })
-                .setImage(summary.avatar.large)
-                .setTimestamp()
-            msg.channel.send(embed)
+function sendInfoLink(msg, userLink) {
+    steamApi.getId64(userLink).then(id64 => {
+            steamApi.getInfos(id64).then(summary => {
+                console.log(summary)
+                msg.channel.send(createEmbed(summary))
+            })
         })
-        .catch(err => console.error(err))
+        .catch(err => {
+            console.error(err)
+            msg.channel.send(err.toString())
+        })
+}
 
-});
+function sendInfoId(msg, id64) {
+    steamApi.getInfos(id64).then(summary => {
+            //  console.log(summary)
+            msg.channel.send(createEmbed(summary))
+        })
+        .catch(err => {
+            console.error(err)
+            msg.channel.send(err.toString())
+        })
+}
+
+function createEmbed(summary) {
+    return new discord.MessageEmbed()
+        .setColor('#ff0000')
+        .setTitle(summary.nickname)
+        .setURL(summary.url)
+        .addFields({
+            name: 'Id64',
+            value: summary.steamID,
+            inline: true
+        }, {
+            name: 'VAC BAN',
+            value: summary.bans.vacBanned,
+            inline: true
+
+        }, {
+            name: 'GAME BAN',
+            value: summary.bans.gameBans >= 1 ? true : false,
+            inline: true
+
+        }, {
+            name: 'Days since last ban:',
+            value: summary.bans.daysSinceLastBan,
+            inline: true
+
+        }, {
+            name: 'Community ban',
+            value: summary.bans.communityBanned,
+            inline: true
+
+        }, {
+            name: 'Economy ban:',
+            value: summary.bans.economyBan,
+            inline: true
+        })
+        .setImage(summary.avatar.large)
+        .setTimestamp()
+}
+
+function watchadd(msg) {
+    let userLink = msg.content.split(" ")[1]
+    if (!userLink) return
+    steamApi.getId64(userLink).then(id => {
+            steamApi.isBan(id).then(isBan => {
+                db.prepare('insert or ignore into STEAM_BAN_MONITOR(ID_64, DATE_START_MONITORING, BANNED) values(?, ?, ?)').run(id, Date.now(), isBan ? '1' : '0')
+                msg.react('🟢')
+            })
+        })
+        .catch(err => {
+            console.error(err)
+            msg.react('🔴')
+            msg.channel.send(err.toString())
+        })
+}
+
+function watchrm(msg) {
+    let userLink = msg.content.split(" ")[1]
+    if (!userLink) return
+    steamApi.getId64(userLink).then(id => {
+            db.prepare('delete from STEAM_BAN_MONITOR where ID_64 = ?').run(id)
+            msg.react('🟢')
+        })
+        .catch(err => {
+            console.error(err)
+            msg.react('🔴')
+            msg.channel.send(err.toString())
+        })
+}
+
+function watchls(msg) {
+    let ids = db.prepare('select ID_64 from STEAM_BAN_MONITOR').all()
+    console.log(ids)
+    ids.forEach(id => {
+        sendInfoId(msg, id.ID_64)
+    })
+}
+
+function banls(msg) {
+    let ids = db.prepare('select ID_64 from STEAM_BAN_MONITOR where BANNED = 1').all()
+    console.log(ids)
+    if (ids.length == 0)
+        msg.channel.send('Aucun joueurs bannis :(')
+    ids.forEach(id => {
+        sendInfoId(msg, id.ID_64)
+    })
+}
+
+function lookForBans() {
+    let ids = db.prepare('select ID_64 from STEAM_BAN_MONITOR where BANNED = 0').all()
+    ids.forEach(id => {
+        steamApi.isBan(id.ID_64).then(isBan => {
+                db.prepare('update STEAM_BAN_MONITOR set BANNED = ?, BANNED_DATE = ? where ID_64 = ?').run(isBan ? '1' : '0', isBan ? Date.now() : null, id.ID_64)
+            })
+            .catch(err => console.error(err))
+    })
+}
+
+function banAlert() {
+    let ids = db.prepare('select ID_64, BANNED_DATE from STEAM_BAN_MONITOR where BANNED = 1 and BANNED_DATE - ? <= 60000').all(Date.now())
+    ids.forEach(id => {
+        // 607903859934756892 channelId de generale  323894435203121172 matpute id
+        let guild = discordClient.guilds.cache.get('323894435203121172')
+        let chan = guild.channels.cache.get('607903859934756892')
+        chan.send('🚨 ***UN BANNISSEMENT*** 🚨')
+        steamApi.getInfos(id.ID_64).then(summary => {
+                chan.send(createEmbed(summary))
+                chan.send('🚨 ***FIN ALERTE*** 🚨')
+            })
+            .catch(err => {
+                console.error(err)
+                chan.send(err.toString())
+            })
+
+    })
+}
+
+function banCheck() {
+    lookForBans()
+    banAlert()
+}
--- src/steam.js
+++ src/steam.js
@@ -3,9 +3,33 @@ const apiKey = require('../apiKey.json')
 const SteamAPI = require('steamapi');
 const steam = new SteamAPI(apiKey.steam);
 
-
-exports.getInfos = async (userLink) => {
+async function getId64(userLink) {
     let id = await steam.resolve(userLink)
-    let summary = await steam.getUserSummary(id)
+    return id
+}
+
+async function getInfos(id64) {
+    let summary = await steam.getUserSummary(id64)
+    summary.bans = await getBans(id64)
     return summary
 }
+
+async function getBans(id64) {
+    let ban = await steam.getUserBans(id64)
+    return ban
+}
+
+async function isBan(id64) {
+    let ret = false;
+    let ban = await steam.getUserBans(id64)
+    if (ban.vacBanned)
+        ret = true
+    if (ban.gameBans >= 1)
+        ret = true
+    return ret
+}
+
+exports.isBan = isBan
+exports.getId64 = getId64
+exports.getInfos = getInfos
+exports.getBan = getBans
